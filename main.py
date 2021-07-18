db6058c051f401e4c951a3609d8504b7e453205e
Marsden Jacques <marsdensgardens@gmail.com>
2021-07-19 01:08:44 +0900

    added file detection/opening players for every video file in input folder

--- main.py
+++ main.py
@@ -5,19 +5,29 @@ import mpv
 from time import sleep
 
 running = True
-player = mpv.MPV()
-player.play('2.mp4')
 
-@player.on_key_press('q')
-def my_q_binding():
-    player.stop()
-@player.on_key_press('p')
-def my_q_binding():
-    paused = player._get_property('pause')
-    player._set_property('pause', not paused)
-player._set_property('loop', False)
-print(player._get_property('loop'))
-player.wait_for_playback()
+player = mpv.MPV()
+player2 = mpv.MPV()
+players = []
+vidsList = os.listdir('vids/')
+for vids in vidsList:
+    player = mpv.MPV()
+    @player.on_key_press('q')
+    def my_q_binding():
+        player.stop()
+    @player.on_key_press('p')
+    def my_q_binding():
+        paused = player._get_property('pause')
+        player._set_property('pause', not paused)
+    player._set_property('loop', False)
+    players.append(player)
+index = 0
+while index < len(players):
+    players[index].play('vids/' + vidsList[index])
+    index = index + 1
+sleep(5)
+print(vidsList)
+
 #sleep(2) # Or however long you expect it to take to open vlc
 #while player. & running:
 #    if(keyboard.is_pressed('q')):
