29f35601c0425435d0cf846b7b8ea18d2107ad98
Marsden Jacques <marsdensgardens@gmail.com>
2021-07-26 17:57:25 +0900

    basic instructions, and finished all toggles, plus some minor bugfixes

--- main.py
+++ main.py
@@ -4,8 +4,13 @@ os.environ["PATH"] = os.path.dirname('.\lib\mpv-1.dll') + os.pathsep + os.enviro
 import mpv
 from time import sleep
 
-def playerLoop(players):
-    print('x')
+def toggle(players, property):
+    index = 0
+    while index < len(players):
+        toggle = players[index]._get_property(property)
+        players[index]._set_property(property, not toggle)
+        index = index + 1
+    sleep(.1)
 
 running = True
 player = mpv.MPV()
@@ -15,9 +20,11 @@ vidsList = os.listdir('vids/')
 for vids in vidsList:
     player = mpv.MPV()
     player._set_property('loop', True)
-    player._set_property('fullscreen', True)
+    player._set_property('fullscreen', False)
     player._set_property('snap-window', True)
+    player._set_property('osc', True)
     player._set_property('border', False)
+    player._set_property('keep-open', True)
     players.append(player)
 index = 0
 while index < len(players):
@@ -27,24 +34,25 @@ while(running):
     if(keyboard.is_pressed('Enter')):
         index = 0
         while index < len(players):
-            players[index].seek(0)
-            if(players[index]._get_property('pause')):
-                players[index]._set_property('pause', False)
+            players[index].seek(0,'absolute')
+            players[index]._set_property('pause', True)
             index = index + 1
         sleep(.1)
+    if(keyboard.is_pressed('l')):
+        toggle(players,'loop')
     if(keyboard.is_pressed('f')):
-        index = 0
-        while index < len(players):
-            fullscreen = players[index]._get_property('fullscreen')
-            players[index]._set_property('fullscreen', not fullscreen)
-            index = index + 1
-        sleep(.1)
+        toggle(players,'fullscreen')
     if(keyboard.is_pressed('Space')):
-        index = 0
-        while index < len(players):
-            paused = players[index]._get_property('pause')
-            players[index]._set_property('pause', not paused)
-            index = index + 1
-        sleep(.1)
+        toggle(players,'pause')
+    if(keyboard.is_pressed('b')):
+        toggle(players,'border')
+    if(keyboard.is_pressed('s')):
+        toggle(players,'snap-window')
+    if(keyboard.is_pressed('c')):
+        toggle(players,'osc')
+    if(keyboard.is_pressed('k')):
+        toggle(players,'keep-open')
     if(keyboard.is_pressed('Escape')):
         running = False
+
+        #make toggle, button for osc, snap, and border
---
+++ readme.txt
@@ -0,0 +1,12 @@
+add a "vids" folder, and place all videos inside of it
+add a "lib" folder, download mpv-dev from here: https://sourceforge.net/projects/mpv-player-windows/ and extract the files to the lib folder
+
+Space = pause/play
+Escape = quit
+Enter = restart videos
+f = toggle fullscreen
+l = toggle loop
+b = toggle border
+s = toggle window snap
+c = toggle on screen controller
+k = toggle stay open on finish
