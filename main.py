344530cfdc94c2f0d180bae808a8a099c79595de
Marsden Jacques <marsdensgardens@gmail.com>
2021-07-26 17:20:22 +0900

    added pausing, restarting, fullscreen, closing. Maybe I'll add ability to toggle looping next?

--- main.py
+++ main.py
@@ -1,29 +1,50 @@
 import keyboard
 import os
-os.environ["PATH"] = os.path.dirname('D:\Creative\Marsden\Coding Adventures\Python\SimultaneousVideoPlayer\lib\mpv-1.dll') + os.pathsep + os.environ["PATH"]
+os.environ["PATH"] = os.path.dirname('.\lib\mpv-1.dll') + os.pathsep + os.environ["PATH"]
 import mpv
 from time import sleep
 
-running = True
+def playerLoop(players):
+    print('x')
 
+running = True
 player = mpv.MPV()
 player2 = mpv.MPV()
 players = []
 vidsList = os.listdir('vids/')
 for vids in vidsList:
     player = mpv.MPV()
-    @player.on_key_press('q')
-    def my_q_binding():
-        player.stop()
-    @player.on_key_press('p')
-    def my_q_binding():
-        paused = player._get_property('pause')
-        player._set_property('pause', not paused)
-    player._set_property('loop', False)
+    player._set_property('loop', True)
+    player._set_property('fullscreen', True)
+    player._set_property('snap-window', True)
+    player._set_property('border', False)
     players.append(player)
 index = 0
 while index < len(players):
     players[index].play('vids/' + vidsList[index])
     index = index + 1
-sleep(5)
-print(vidsList)
+while(running):
+    if(keyboard.is_pressed('Enter')):
+        index = 0
+        while index < len(players):
+            players[index].seek(0)
+            if(players[index]._get_property('pause')):
+                players[index]._set_property('pause', False)
+            index = index + 1
+        sleep(.1)
+    if(keyboard.is_pressed('f')):
+        index = 0
+        while index < len(players):
+            fullscreen = players[index]._get_property('fullscreen')
+            players[index]._set_property('fullscreen', not fullscreen)
+            index = index + 1
+        sleep(.1)
+    if(keyboard.is_pressed('Space')):
+        index = 0
+        while index < len(players):
+            paused = players[index]._get_property('pause')
+            players[index]._set_property('pause', not paused)
+            index = index + 1
+        sleep(.1)
+    if(keyboard.is_pressed('Escape')):
+        running = False
